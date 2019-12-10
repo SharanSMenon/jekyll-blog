b007cfeba75975aec6b7434f87b12339890def84
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2019-12-10 10:31:16 -0500

    Commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Welcome to my blog. This is my first post and I will be adding more posts soon. Check the about page if you want to learn more about me.   That’s it for now and I will see you later   Bye!  ","categories": ["Misc"],
+        "tags": ["First Post"],
+        "url": "/misc/first-post/",
+        "teaser":null}]
