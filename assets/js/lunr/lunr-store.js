b380708d954f728d33971f0fa663e38f20d32110
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2019-12-13 09:00:37 -0500

    Added post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,11 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
+        "title": "First Post",
         "excerpt":"Welcome to my blog. This is my first post and I will be adding more posts soon. Check the about page if you want to learn more about me.   That’s it for now and I will see you later   Bye!  ","categories": ["Misc"],
         "tags": ["First Post"],
         "url": "/misc/first-post/",
+        "teaser":null},{
+        "title": "Calculating Jacobian Vectors",
+        "excerpt":"Jacobian vectors are a really powerful tool in mathematics, and it is very useful in machine learning. Learning how to calculate Jacobian vectors will be important to understand machine learning, so let’s learn how to calculate it. You need to know what a vector is and how to take the...","categories": ["Mathematics"],
+        "tags": ["Linear Algebra","Calculus"],
+        "url": "/mathematics/jacobian-vectors/",
         "teaser":null}]
