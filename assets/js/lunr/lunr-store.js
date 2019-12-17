aa90e80efaa1e894fc5945e091a331f27621465e
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2019-12-17 15:11:34 -0500

    Added post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,15 +2,20 @@ var store = [{
         "title": "First Post",
         "excerpt":"Welcome to my blog. This is my first post and I will be adding more posts soon. Check the about page if you want to learn more about me.   That’s it for now and I will see you later   Bye!  ","categories": ["Misc"],
         "tags": ["First Post"],
-        "url": "/misc/first-post/",
+        "url": "http://localhost:4000/misc/first-post/",
         "teaser":null},{
         "title": "Calculating Jacobian Vectors",
         "excerpt":"Jacobian vectors are a really powerful tool in mathematics, and it is very useful in machine learning. Learning how to calculate Jacobian vectors will be important to understand machine learning, so let’s learn how to calculate it. You need to know what a vector is and how to take the...","categories": ["Mathematics"],
         "tags": ["Linear Algebra","Calculus"],
-        "url": "/mathematics/jacobian-vectors/",
+        "url": "http://localhost:4000/mathematics/jacobian-vectors/",
         "teaser":null},{
         "title": "Calculus with **Sympy**",
         "excerpt":"There is a python library called SymPy. It is a python library for symbolic mathematics (means that it can work with symbols like x, y, and z). This library can do calculus which means that it can find limits, derivatives, and integrate functions. It can also do many other things...","categories": ["Mathematics","Programming"],
         "tags": ["Python","Calculus"],
-        "url": "/mathematics/programming/calculus-with-sympy/",
+        "url": "http://localhost:4000/mathematics/programming/calculus-with-sympy/",
+        "teaser":null},{
+        "title": "MNIST with Tensorflow",
+        "excerpt":"Tensorflow is a powerful deep learning framework developed by the team at Google. It is used to build highly accurate neural networks. This is probably my last post of the year (for future readers, the year that I am writing this in is 2019). We will use Tensorflow (it’s Keras...","categories": ["Programming","Machine Learning"],
+        "tags": ["Python","Tensorflow"],
+        "url": "http://localhost:4000/programming/machine%20learning/Fashion-MNIST-with-PyTorch/",
         "teaser":null}]
