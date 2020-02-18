f9902df472e2d7e1f0b57ef5b35cf0c374b05892
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2020-02-18 11:05:31 -0500

    New Posts and changes

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,25 +2,35 @@ var store = [{
         "title": "First Post",
         "excerpt":"Welcome to my blog. This is my first post and I will be adding more posts soon. Check the about page if you want to learn more about me.   That’s it for now and I will see you later   Bye!  ","categories": ["Misc"],
         "tags": ["First Post"],
-        "url": "http://localhost:4000/misc/first-post/",
+        "url": "/misc/first-post/",
         "teaser":null},{
         "title": "Calculating Jacobian Vectors",
         "excerpt":"Jacobian vectors are a really powerful tool in mathematics, and it is very useful in machine learning. Learning how to calculate Jacobian vectors will be important to understand machine learning, so let’s learn how to calculate it. You need to know what a vector is and how to take the...","categories": ["Mathematics"],
         "tags": ["Linear Algebra","Calculus"],
-        "url": "http://localhost:4000/mathematics/jacobian-vectors/",
+        "url": "/mathematics/jacobian-vectors/",
         "teaser":null},{
         "title": "Calculus with **Sympy**",
         "excerpt":"There is a python library called SymPy. It is a python library for symbolic mathematics (means that it can work with symbols like x, y, and z). This library can do calculus which means that it can find limits, derivatives, and integrate functions. It can also do many other things...","categories": ["Mathematics","Programming"],
         "tags": ["Python","Calculus"],
-        "url": "http://localhost:4000/mathematics/programming/calculus-with-sympy/",
+        "url": "/mathematics/programming/calculus-with-sympy/",
         "teaser":null},{
         "title": "MNIST with Tensorflow",
         "excerpt":"Tensorflow is a powerful deep learning framework developed by the team at Google. It is used to build highly accurate neural networks. This is probably my last post of the year (for future readers, the year that I am writing this in is 2019). We will use Tensorflow (it’s Keras...","categories": ["Programming","Machine Learning"],
-        "tags": ["Python","Tensorflow"],
-        "url": "http://localhost:4000/programming/machine%20learning/Fashion-MNIST-with-PyTorch/",
+        "tags": ["Python","Images","Tensorflow"],
+        "url": "/programming/machine%20learning/Fashion-MNIST-with-PyTorch/",
         "teaser":null},{
         "title": "CNNs in PyTorch",
         "excerpt":"Introduction PyTorch is a deep learning library developed by Facebook. It is a powerful library and can build highly accurate deep learning models. We will build a Convolutional Neural Network today. We will be training our model on the CIFAR-10 dataset which is a dataset of many 32 x 32...","categories": ["Programming","Machine Learning"],
-        "tags": ["Python","PyTorch"],
-        "url": "http://localhost:4000/programming/machine%20learning/cnn-pytorch/",
+        "tags": ["Python","Images","PyTorch"],
+        "url": "/programming/machine%20learning/cnn-pytorch/",
+        "teaser":null},{
+        "title": "Flower Recognition in PyTorch",
+        "excerpt":"Welcome to this tutorial where you will learn to use transfer learning to classify images of flowers with pytorch. We will be using the VGG16 model in the tutorial so follow along. Getting the Data Here is the link to the dataset. If you have a folder called flowers in...","categories": ["Programming","Machine Learning"],
+        "tags": ["Python","Images","PyTorch"],
+        "url": "/programming/machine%20learning/flowers-recognition/",
+        "teaser":null},{
+        "title": "Linear Regression in PyTorch",
+        "excerpt":"Hello and welcome to this tutorial where we learn a quadratic equation in PyTorch. We will train a neural network to predict future values of the function $y=2x$. So given an $x$, the network will have to predict $y$. This is similar to a previous tutorial that I made which...","categories": ["Programming","Machine Learning"],
+        "tags": ["Python","PyTorch","Regression"],
+        "url": "/programming/machine%20learning/linear-regression-pytorch/",
         "teaser":null}]
